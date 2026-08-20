1964d2d240db46f33bc3acf3041e4ea28348c4ab
joaooliveira247 <joaooliveira247@gmail.com>
2026-08-20 18:29:37 -0300

    feat(cmd/tui): add footerLayout func to represente footer section in TUI

--- cmd/tui.go
+++ cmd/tui.go
@@ -2,6 +2,14 @@ package cmd
 
 import "github.com/rivo/tview"
 
+func footerLayout() *tview.TextView {
+	footer := tview.NewTextView().
+		SetDynamicColors(true).
+		SetTextAlign(tview.AlignCenter).
+		SetText("[black:yellow] F1 [-:-] Quit")
+	return footer
+}
+
 func RootLayout() *tview.Flex {
 	root := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(tview.NewBox().SetBorder(true).SetTitle("Content"), 0, 1, true).
