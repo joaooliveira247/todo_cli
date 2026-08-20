67e690cbc46e063ef247fcdcc5cba6803a3f9591
joaooliveira247 <joaooliveira247@gmail.com>
2026-08-20 18:12:02 -0300

    feat(cmd/root): add Run method for App struct to run TUI

--- cmd/root.go
+++ cmd/root.go
@@ -8,6 +8,13 @@ type App struct {
 	tviewApp *tview.Application
 }
 
+func (app *App) Run() error {
+	if err := app.tviewApp.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
 func NewApp() *App {
 	app := &App{tviewApp: tview.NewApplication()}
 
