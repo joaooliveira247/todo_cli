0086a36761cae1613363dd86dd66c08ba78b5315
joaooliveira247 <joaooliveira247@gmail.com>
2026-08-20 18:32:56 -0300

    feat(cmd/root): add keyPressEvent method of App struct to manage TUI app

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,22 @@
 package cmd
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 type App struct {
 	tviewApp *tview.Application
+}
+
+func (app *App) keyPressEvent(event *tcell.EventKey) *tcell.EventKey {
+	switch event.Key() {
+	case tcell.KeyF1:
+		app.tviewApp.Stop()
+		return nil
+	}
+
+	return event
 }
 
 func (app *App) Run() error {
