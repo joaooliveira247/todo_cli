742ca2f87a9a0d224280ebcda16368ea8ee3d180
joaooliveira247 <joaooliveira247@gmail.com>
2026-08-27 21:03:40 -0300

    feat(tui/widgets/table): add buildHeader to write header in table

--- internal/tui/widgets/table.go
+++ internal/tui/widgets/table.go
@@ -1,6 +1,7 @@
 package widgets
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
@@ -14,4 +15,20 @@ type TableWidget struct {
 func NewTableWidget() *TableWidget {
 	var testMap []map[string]string
 	return &TableWidget{tview.NewTable(), false, 0, testMap}
+}
+
+func (tw *TableWidget) buildHeader(items []string) {
+	if tw.tableRows > 0 {
+		tw.Table.Clear()
+	}
+
+	for col, item := range items {
+		cell := tview.NewTableCell(item).
+			SetTextColor(tcell.ColorWhite).
+			SetSelectable(false).
+			SetAlign(tview.AlignCenter).
+			SetExpansion(1)
+
+		tw.Table.SetCell(0, col, cell)
+	}
 }
