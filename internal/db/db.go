91de912561c503fafde927e4343ee7767c59675c
joaooliveira247 <joaooliveira247@gmail.com>
2026-08-28 21:10:11 -0300

    feat(internal/db): add getDatabasePath func to get current database file path or create it

---
+++ internal/db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func getDatabasePath() (string, error) {
+	// only for linux, maybe later for others OS
+	dataDir := os.Getenv("XDG_DATA_HOME")
+
+	if dataDir == "" {
+		home, err := os.UserHomeDir()
+
+		if err != nil {
+			return "", err
+		}
+		dataDir = filepath.Join(home, ".local", "share")
+	}
+
+	appDir := filepath.Join(dataDir, "todo-cli")
+
+	if err := os.Mkdir(appDir, 0755); err != nil {
+		return "", err
+	}
+
+	return filepath.Join(appDir, "todo.db"), nil
+}
