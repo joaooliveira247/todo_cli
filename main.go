578ecf8bfa596a50ab993b399289a08f92d6674e
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-02 16:57:31 -0300

    feat(main): add instance of sql.DB in main file, and pass it to NewApp func

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"github.com/joaooliveira247/todo_cli/cmd"
+	"github.com/joaooliveira247/todo_cli/internal/db"
 )
 
 func main() {
-	app := cmd.NewApp()
+	database, err := db.InitDB()
+	defer database.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	app := cmd.NewApp(database)
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
