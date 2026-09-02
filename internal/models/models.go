89b0610767a98e7b2dc5db639c1190fa42ebd32c
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-02 11:45:01 -0300

    refactor(internal/models): remove ID field creation in NewTaskModel func

--- internal/models/models.go
+++ internal/models/models.go
@@ -2,8 +2,6 @@ package models
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 const (
@@ -22,7 +20,6 @@ type TaskModel struct {
 
 func NewTaskModel(task string) *TaskModel {
 	return &TaskModel{
-		ID:   uuid.NewString(),
 		Task: task,
 	}
 }
