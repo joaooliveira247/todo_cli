83123257743ebe36cbc2d1391704bd3a5513551a
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-03 08:45:45 -0300

    feat(utils/date): add GetCurrentPeriod func

--- internal/utils/date.go
+++ internal/utils/date.go
@@ -5,3 +5,11 @@ import "time"
 func FormatDate(date time.Time) string {
 	return date.Format("02/01/2006")
 }
+
+func GetCurrentPeriod() (time.Time, time.Time) {
+	currentDate := time.Now()
+
+	startPerdiod := currentDate.AddDate(0, 0, -int(currentDate.Weekday()))
+	endPeriod := currentDate.AddDate(0, 0, (6 - int(currentDate.Weekday())))
+	return startPerdiod, endPeriod
+}
