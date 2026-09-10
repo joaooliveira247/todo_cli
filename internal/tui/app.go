ba6298003d841364397766811b95b15513f23827
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-10 14:58:26 -0300

    refactor(tui/app): change text in footerLayout now show F1 to add task

--- internal/tui/app.go
+++ internal/tui/app.go
@@ -73,7 +73,7 @@ func (ui *AppUI) footerLayout() *tview.TextView {
 	footer := tview.NewTextView().
 		SetDynamicColors(true).
 		SetTextAlign(tview.AlignCenter).
-		SetText("[black:yellow] F4 [-:-] Quit")
+		SetText("[black:yellow] F1 [-:-] Add task [black:yellow] F4 [-:-] Quit")
 	return footer
 }
 
