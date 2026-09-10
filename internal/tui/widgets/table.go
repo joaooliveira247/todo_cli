20f6d4a0ee0c4001c780c1d24907f57a2bbaad5a
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-10 14:33:25 -0300

    feat(widgets/table): add UpdateRow method in TableWidget struct

--- internal/tui/widgets/table.go
+++ internal/tui/widgets/table.go
@@ -23,7 +23,7 @@ func NewTableWidget(
 	repository *repositories.TaskRepository,
 	modals *modals.Modals,
 ) *TableWidget {
-	//TODO: fix iniPeriod, and error handling here
+	//TODO: fix iniPeriod, and error handling here, maybe create a method to return or write data 'cause if reload table Data don't update
 	data, _ := repository.GetTasks(time.Now(), false)
 	return &TableWidget{tview.NewTable(), false, 0, data, repository, modals}
 }
@@ -51,6 +51,16 @@ func (tw *TableWidget) AddRow(rowIdx int, row *models.TaskRow) {
 	tw.Table.SetCell(rowIdx, 2, row.CreatedAt)
 	tw.Table.SetCell(rowIdx, 3, row.UpdatedAt)
 	tw.Table.SetCell(rowIdx, 4, row.Status)
+}
+
+func (tw *TableWidget) UpdateRow(row int, task *models.TaskModel) {
+	if tw.ShowConcludedTasks {
+		tw.AddRow(row, task.ToRow())
+		return
+	}
+	tw.Table.RemoveRow(row)
+	tw.tableRows--
+	return
 }
 
 func (tw *TableWidget) AddTask(task *models.TaskModel) {
