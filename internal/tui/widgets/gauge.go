a4a57e33a9be052b57cdcdb26d2ca453c1c3087d
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-12 21:53:07 -0300

    feat(widgets/gauge): add method Draw in GaugeView struct

--- internal/tui/widgets/gauge.go
+++ internal/tui/widgets/gauge.go
@@ -1,6 +1,9 @@
 package widgets
 
 import (
+	"fmt"
+
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
@@ -25,4 +28,47 @@ func (g *GaugeView) SetPercent(percent int) *GaugeView {
 	}
 	g.percent = percent
 	return g
+}
+
+func (g *GaugeView) Draw(screen tcell.Screen) {
+	g.Box.DrawForSubclass(screen, g)
+
+	x, y, width, height := g.GetInnerRect()
+	if width <= 0 || height <= 0 {
+		return
+	}
+
+	filledHeight := (height * g.percent) / 100
+
+	for row := 0; row < height; row++ {
+		currentY := y + height - 1 - row
+
+		style := tcell.StyleDefault.Foreground(tcell.ColorDarkGray).
+			Background(tcell.ColorBlack)
+		ch := '░'
+
+		if row < filledHeight {
+			style = tcell.StyleDefault.Foreground(tcell.ColorGreen).
+				Background(tcell.ColorBlack)
+			ch = '█'
+		}
+
+		for col := 0; col < width; col++ {
+			screen.SetContent(x+col, currentY, ch, nil, style)
+		}
+	}
+
+	text := fmt.Sprintf("%d%%", g.percent)
+	textLen := len(text)
+	textStyle := tcell.StyleDefault.Foreground(tcell.ColorYellow).
+		Background(tcell.ColorBlack).
+		Bold(true)
+
+	if textLen <= width {
+		centerX := x + (width-textLen)/2
+		bottomY := y + height - 1 // Última linha disponível
+		for i, char := range text {
+			screen.SetContent(centerX+i, bottomY, char, nil, textStyle)
+		}
+	}
 }
