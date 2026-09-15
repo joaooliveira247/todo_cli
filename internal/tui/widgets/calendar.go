da1e4559ab820d49ac525287b14e2b4619b8a49a
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-15 17:16:29 -0300

    feat(widgets/calendar) add NewCalendar func

--- internal/tui/widgets/calendar.go
+++ internal/tui/widgets/calendar.go
@@ -3,6 +3,7 @@ package widgets
 import (
 	"time"
 
+	"github.com/joaooliveira247/todo_cli/internal/utils"
 	"github.com/rivo/tview"
 )
 
@@ -16,4 +17,20 @@ type Calendar struct {
 	StartPeriod time.Time
 	EndPeriod   time.Time
 	CurrentDate time.Time
+}
+
+func NewCalendar(app *tview.Application) *Calendar {
+	startPeriod, EndPeriod := utils.GetCurrentPeriod()
+	c := &Calendar{
+		tview.NewFlex().SetDirection(tview.FlexRow),
+		app,
+		nil,
+		nil,
+		nil,
+		startPeriod,
+		EndPeriod,
+		time.Now(),
+	}
+	c.buildCalendar()
+	return c
 }
