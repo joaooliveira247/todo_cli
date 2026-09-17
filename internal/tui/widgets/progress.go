e39724a56f031d5c4b55607daf61e9010da06bbb
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-17 18:00:15 -0300

    feat(widgets/progress): add func NewProgress

--- internal/tui/widgets/progress.go
+++ internal/tui/widgets/progress.go
@@ -9,3 +9,14 @@ type Progress struct {
 	Gauge        *GaugeView
 	commits      *CommitWidget
 }
+
+func NewProgress() *Progress {
+	p := &Progress{
+		tview.NewFlex().SetDirection(tview.FlexRow),
+		NewGauge(),
+		NewCommitWidget(),
+	}
+	p.ProgressView.AddItem(p.Gauge, 0, 1, false).
+		AddItem(p.commits, 4, 1, false)
+	return p
+}
