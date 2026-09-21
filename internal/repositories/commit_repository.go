730eecc66bc82d4fa1a0b918798e808d50453174
joaooliveira247 <joaooliveira247@gmail.com>
2026-09-20 21:03:26 -0300

    feat(repositories/commit): start development of GetCommitsCompleted method in CommitRepository struct, it may change

--- internal/repositories/commit_repository.go
+++ internal/repositories/commit_repository.go
@@ -85,3 +85,33 @@ func (cr *CommitRepository) GetCommits(
 
 	return commits, nil
 }
+
+// INFO: Here u can return commits that isn't marked as completed true or in update add logic to safe last out of period
+// INFO: maybe only return dates
+func (cr *CommitRepository) GetCommitsCompleted(
+	iniPeriod time.Time,
+) ([]time.Time, error) {
+	query := `SELECT date FROM commits WHERE date >= ? AND is_completed = true;`
+
+	rows, err := cr.db.Query(query, iniPeriod.Format("02-01-2006"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var dates []time.Time
+
+	for rows.Next() {
+		var date time.Time
+
+		if err := rows.Scan(&date); err != nil {
+			return nil, err
+		}
+
+		dates = append(dates, date)
+	}
+
+	return dates, nil
+}
